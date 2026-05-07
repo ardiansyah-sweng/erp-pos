b44699ba28d7030b660e66d9ffce7f896e76613d
cheorry <reysajuni22@gmail.com>
2026-05-08 00:05:24 +0700

    Search Casier

---
+++ app/Http/Controllers/CashierSearchController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CashierService; // Mengarah ke app/Services
+use Illuminate\Http\Request;
+
+class CashierSearchController extends Controller
+{
+    protected $cashierService;
+
+    public function __construct(CashierService $cashierService)
+    {
+        $this->cashierService = $cashierService;
+    }
+
+   public function search(Request $request)
+{
+    $keyword = $request->query('q');
+    $results = $this->cashierService->findCashier($keyword);
+
+    // Kirim data ke file view index
+    return view('cashier.index', ['cashiers' => $results]);
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ScheduleController;
+// 1. Tambahkan import controller baru kamu di sini
+use App\Http\Controllers\CashierSearchController; 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 2. Ubah rute '/' agar tidak ke 'welcome' lagi, tapi ke Controller Search
+Route::get('/', [CashierSearchController::class, 'search']);
 
 Route::get('/jalankan-schedule', [ScheduleController::class, 'index'])->name('jalankan-schedule-index');
 Route::post('/jalankan-schedule', [ScheduleController::class, 'run'])->name('jalankan-schedule');
 Route::get('/products', [ProductController::class, 'getProducts']);
 Route::get('/transactions', [TransactionController::class, 'getTransaction']);
+
+// 3. (Opsional) Kamu juga bisa buat rute spesifik untuk search
+Route::get('/cashier/search', [CashierSearchController::class, 'search'])->name('cashier.search');
