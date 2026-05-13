0e37f73c6e5064dc8df61559363bc175a4b24705
TanjungPermata <tanjungdn24@gmail.com>
2026-05-13 15:05:03 +0700

    nambahin router

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PosController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return redirect()->route('pos.index');
@@ -17,3 +18,4 @@ Route::post('/jalankan-schedule', [ScheduleController::class, 'run'])->name('jal
 Route::get('/products', [ProductController::class, 'getProducts'])->name('products.index');
 Route::get('/transactions', [TransactionController::class, 'getTransaction'])->name('transactions.index');
 Route::post('/pos/checkout', [TransactionController::class, 'checkout'])->name('pos.checkout');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
