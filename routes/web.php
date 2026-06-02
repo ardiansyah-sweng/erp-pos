1c0a0b860514b105b5ffa04638a07a552084f1b5
TanjungPermata <tanjungdn24@gmail.com>
2026-06-02 13:23:46 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,4 @@ Route::get('/jalankan-schedule', [ScheduleController::class, 'index'])->name('ja
 Route::post('/jalankan-schedule', [ScheduleController::class, 'run'])->name('jalankan-schedule');
 Route::get('/products', [ProductController::class, 'getProducts'])->name('products.index');
 Route::get('/transactions', [TransactionController::class, 'getTransaction'])->name('transactions.index');
-Route::post('/pos/checkout', [TransactionController::class, 'checkout'])->name('pos.checkout');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
