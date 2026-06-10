2528d13b5e0b18b27f6569b670066f736e82988f
Revi <repiiadelia@gmail.com>
2026-06-10 16:20:54 +0700

    melanjautkan checkout

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::post('/jalankan-schedule', [ScheduleController::class, 'run'])->name('jal
 Route::get('/products', [ProductController::class, 'getProducts'])->name('products.index');
 Route::get('/transactions', [TransactionController::class, 'getTransaction'])->name('transactions.index');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/transaction/store', [TransactionController::class, 'store']);
 Route::post('/pos/checkout', [TransactionController::class, 'checkout']);
