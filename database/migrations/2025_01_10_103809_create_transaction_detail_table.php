ea756d12b38639aaf52bfc185d9bfc9cd740f860
RudhyMasyaid <2300018215@webmail.uad.ac.id>
2026-06-23 16:01:58 +0700

    modifikasi menghapus transaction_date di transaction detail

--- database/migrations/2025_01_10_103809_create_transaction_detail_table.php
+++ database/migrations/2025_01_10_103809_create_transaction_detail_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('quantity');
             $table->integer('price');
             $table->integer('amount');
-            $table->timestamp('transaction_date')->nullable();
             $table->timestamps();
         });
     }
