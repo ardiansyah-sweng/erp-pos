62978036ecdf9bae4d6f0269648f188ed888b9f9
SitriNurulH <sittinurul64@gmail.com>
2026-06-25 20:29:40 +0700

    cleanup: revert semua file yang bukan bagian tugas ke versi develop
    Hapus .github/workflows/laravel.yml dan package-lock.json yang tidak
    ada di develop. Reset file lain ke versi develop agar PR hanya berisi
    perubahan terkait tugas membuat tabel products.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
         $this->call([
