014e0a9701f721064140a63ca9ebea66b2eab49a
SyafieqMKS <syafiqsanudin@gmail.com>
2026-06-30 15:58:01 +0700

    Resolve merge conflict in routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PosController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\StockAdjustmentController;
 use App\Http\Controllers\TransactionController;
@@ -25,7 +27,13 @@ Route::get('/stock-adjustments', [StockAdjustmentController::class, 'index'])
 Route::put('/stock-adjustments/{product}', [StockAdjustmentController::class, 'update'])
     ->name('stock-adjustments.update');
 
+Route::get('/transactions/export/csv', [TransactionController::class, 'exportCsv'])->name('transactions.export');
 Route::get('/transactions', [TransactionController::class, 'getTransaction'])->name('transactions.index');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/pos/checkout', [TransactionController::class, 'checkout'])->name('pos.checkout');
 Route::post('/transaction/store', [TransactionController::class, 'store']);
+
+Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+Route::get('/profile', [ProfileController::class, 'showProfile'])->name('profile.show');
+Route::put('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
