39235b760e3f45b27fa8987ab55e779abfbe8f36
SyafieqMKS <syafiqsanudin@gmail.com>
2026-06-30 16:10:32 +0700

    Resolve merge conflict in routes 3

--- routes/web.php
+++ routes/web.php
@@ -18,36 +18,31 @@ Route::get('/pos', [PosController::class, 'index'])->name('pos.index');
 
 Route::get('/jalankan-schedule', [ScheduleController::class, 'index'])
     ->name('jalankan-schedule-index');
+
 Route::post('/jalankan-schedule', [ScheduleController::class, 'run'])
     ->name('jalankan-schedule');
 
 Route::get('/products', [ProductController::class, 'getProducts'])
     ->name('products.index');
+
 Route::get('/products/sku/{sku}', [ProductController::class, 'getItemBySKU']);
 
 Route::get('/stock-adjustments', [StockAdjustmentController::class, 'index'])
     ->name('stock-adjustments.index');
+
 Route::put('/stock-adjustments/{product}', [StockAdjustmentController::class, 'update'])
     ->name('stock-adjustments.update');
 
-Route::get('/transactions/export/csv', [TransactionController::class, 'exportCsv'])
-    ->name('transactions.export');
-Route::get('/transactions', [TransactionController::class, 'getTransaction'])
-    ->name('transactions.index');
+Route::get('/transactions/export/csv', [TransactionController::class, 'exportCsv'])->name('transactions.export');
+Route::get('/transactions', [TransactionController::class, 'getTransaction'])->name('transactions.index');
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])
-    ->name('login');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
-Route::post('/pos/checkout', [TransactionController::class, 'checkout'])
-    ->name('pos.checkout');
+Route::post('/pos/checkout', [TransactionController::class, 'checkout'])->name('pos.checkout');
 Route::post('/transaction/store', [TransactionController::class, 'store']);
 
-Route::get('/reports', [ReportController::class, 'index'])
-    ->name('reports.index');
+Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
 
-Route::get('/profile', [ProfileController::class, 'showProfile'])
-    ->name('profile.show');
-Route::put('/profile/update', [ProfileController::class, 'updateProfile'])
-    ->name('profile.update');
-Route::put('/profile/password', [ProfileController::class, 'updatePassword'])
-    ->name('profile.password');
+Route::get('/profile', [ProfileController::class, 'showProfile'])->name('profile.show');
+Route::put('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
